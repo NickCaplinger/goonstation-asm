b32663db4b52a8242a74eff7b6093d4e17460c9b
Nick Caplinger <the.nick.caplinger@gmail.com>
2022-08-30 21:24:20 -0500

    Clean up code, add tests

--- src/bin/goonasm.rs
+++
@@ -1,9 +0,0 @@
-use goonstation_asm::assemble;
-
-fn main() {
-    let bin = assemble("OEN 0\nSTO 0\nLD 7\nSTO F");
-    dbg!(bin);
-
-    let bin = assemble("OEN 0\nSTO 0\nLD 7\nSTO");
-    dbg!(bin);
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use logos::Logos;
 use thiserror::Error;
 
-#[derive(Error, Debug, PartialEq)]
+#[derive(Error, Debug, PartialEq, Eq)]
 pub enum AssemblerError {
     #[error("Expected operand")]
     ExpectedOperand,
@@ -57,6 +57,9 @@ enum Token {
     #[regex(r"[a-fA-F0-9]", |lex| u8::from_str_radix(lex.slice(), 16))]
     Operand(u8),
 
+    #[regex(r";.*", logos::skip)]
+    Comment,
+
     #[error]
     #[regex(r"[ \t\n\f]+", logos::skip)]
     Error,
@@ -74,11 +77,8 @@ pub fn assemble(input: &str) -> Result<String, AssemblerError> {
         // If we're expecting an operand, make sure this token is one
         if expecting_operand {
             match token {
-                Token::Operand(_) => {
-                    expecting_operand = false;
-                }
+                Token::Operand(_) => {}
                 _ => {
-                    // TODO
                     return Err(AssemblerError::ExpectedOperand);
                 }
             }
@@ -89,25 +89,11 @@ pub fn assemble(input: &str) -> Result<String, AssemblerError> {
             output.push(token_repr);
         }
 
-        match token {
-            Token::Load
-            | Token::LoadComplement
-            | Token::And
-            | Token::AndComplement
-            | Token::Or
-            | Token::OrComplement
-            | Token::ExclusiveNor
-            | Token::Store
-            | Token::StoreComplement
-            | Token::InputEnable
-            | Token::OutputEnable
-            | Token::Jump => expecting_operand = true,
-            _ => expecting_operand = false,
-        }
+        // Flag whether we're expecting an operand as the next token
+        expecting_operand = does_token_require_operand(&token);
     }
 
     if expecting_operand {
-        // TODO
         return Err(AssemblerError::ExpectedOperand);
     }
 
@@ -131,11 +117,27 @@ fn get_token_representation(token: &Token) -> Option<char> {
         Token::Jump => Some('C'),
         Token::Return => Some('D'),
         Token::SkipIfZero => Some('E'),
-        Token::Operand(operand) => {
-            Some(format!("{:X}", operand).chars().next().unwrap())
-        },
-        Token::Error => None,
+        Token::Operand(operand) => Some(format!("{:X}", operand).chars().next().unwrap()),
+        Token::Comment | Token::Error => None,
     }
+}
+
+fn does_token_require_operand(token: &Token) -> bool {
+    matches!(
+        token,
+        Token::Load
+            | Token::LoadComplement
+            | Token::And
+            | Token::AndComplement
+            | Token::Or
+            | Token::OrComplement
+            | Token::ExclusiveNor
+            | Token::Store
+            | Token::StoreComplement
+            | Token::InputEnable
+            | Token::OutputEnable
+            | Token::Jump
+    )
 }
 
 #[cfg(test)]
@@ -143,8 +145,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn handles_simple_program() {
         let bin = assemble("OEN 0 \nSTO 0");
+        assert_eq!(bin, Ok(String::from("B080")));
+    }
+
+    #[test]
+    fn handles_non_zero_operands() {
+        let bin = assemble("OEN 0\nSTO 0\nLD 7\nSTO F");
+        assert_eq!(bin, Ok(String::from("B080178F")));
+    }
+
+    #[test]
+    fn handles_missing_final_operand() {
+        let bin = assemble("OEN 0\nSTO 0\nLD 7\nSTO");
+        assert_eq!(bin, Err(AssemblerError::ExpectedOperand));
+    }
+
+    #[test]
+    fn handles_missing_middle_operand() {
+        let bin = assemble("OEN 0\nSTO \nLD 7\nSTO F");
+        assert_eq!(bin, Err(AssemblerError::ExpectedOperand));
+    }
+
+    #[test]
+    fn handles_comments() {
+        let bin = assemble("OEN 0 ;enable the output because RR is zero, so input 1 (!RR) is 1\nSTO 0 ;store the 0 from RR in output 1, so the unit outputs the signal \"0:0\"");
         assert_eq!(bin, Ok(String::from("B080")));
     }
 }
